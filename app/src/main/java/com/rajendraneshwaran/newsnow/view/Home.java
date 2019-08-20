37a18d11fe3f146fa5bceefb0bba9342f78ecb67
RajendranEshwaran <rajendranmca2010@gmail.com>
2019-08-20 12:20:07 -0400

    news now new application started and writting resuable tool bar

---
+++ app/src/main/java/com/rajendraneshwaran/newsnow/view/Home.java
@@ -0,0 +1,21 @@
+package com.rajendraneshwaran.newsnow.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
+
+import com.rajendraneshwaran.newsnow.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Find the toolbar view inside the activity layout
+//        Toolbar toolbar = (Toolbar)findViewById(R.id.);
+//        setSupportActionBar(toolbar);
+    }
+}
